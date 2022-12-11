ea15ee36c11128eb3473cb82464f67680b45a8b1
xjonwang <xjonwang@gmail.com>
2022-12-11 16:41:58 -0500

    Added ocean instantiation and url

--- scripts/data_publisher.py
+++ scripts/data_publisher.py
@@ -1,7 +1,10 @@
 from pybundlr import pybundlr
+from utils import create_alice_wallet, create_ocean_instance
 
 # test_pred = [1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65, 1274.65]
 ocean = create_ocean_instance()
+wallet = create_alice_wallet(ocean) #you're Alice
 
 file = "../ETH_DATA/test_pred.csv"
-url = pybundlr.fund_and_upload(file, "matic", )
+url = pybundlr.fund_and_upload(file, "matic", wallet.private_key)
+print(f"Your csv url: {url}")
